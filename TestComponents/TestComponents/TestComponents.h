a914650b8239e5c9c88977a805b27314adde5b8b
maningx <ning.ma@smalltreemedia.com>
2022-06-29 13:47:02 +0800

    创建项目。

---
+++ TestComponents/TestComponents/TestComponents.h
@@ -0,0 +1,18 @@
+//
+//  TestComponents.h
+//  TestComponents
+//
+//  Created by maning on 2022/6/28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestComponents.
+FOUNDATION_EXPORT double TestComponentsVersionNumber;
+
+//! Project version string for TestComponents.
+FOUNDATION_EXPORT const unsigned char TestComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestComponents/PublicHeader.h>
+
+
